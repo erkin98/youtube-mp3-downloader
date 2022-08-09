9502e0bc0da14ac481c84d73e6dd6fc4d6375c2e
Erkin <erkin.qarayev_contractor@gaintheory.com>
2022-08-09 23:57:55 +0400

    wrong error handling

--- app/routers/streams.py
+++ app/routers/streams.py
@@ -1,8 +1,10 @@
+import contextlib
 from fastapi import APIRouter, Request, BackgroundTasks
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 from fastapi.responses import HTMLResponse,FileResponse,StreamingResponse
 from pytube import YouTube
+from pytube.exceptions import RegexMatchError
 from io import BytesIO
 import os
 
@@ -18,13 +20,16 @@ def cleanup(temp_file):
 async def read_users(request: Request, background_tasks: BackgroundTasks):
     if request.method == "POST": 
         form = await request.form()
-        if (form["url"] and 'youtube' in form["url"]): 
-            buffer = BytesIO() # Declaring the buffer
-            url = YouTube(form["url"]) # Getting the URL
-            audio = url.streams.filter(mime_type="audio/mp4", abr = '48kbps', only_audio = True).first() # Store the video into a variable
-            file_path = audio.download(filename=f'{audio.title}.mp3')
-            buffer.seek(0)
-            background_tasks.add_task(cleanup, file_path)
+        if (form["url"] and 'youtu' in form["url"]): 
+                buffer = BytesIO() # Declaring the buffer
+                try:
+                    url = YouTube(form["url"]) # Getting the URL
+                    audio = url.streams.filter(mime_type="audio/mp4", abr = '48kbps', only_audio = True).first() # Store the video into a variable
+                    file_path = audio.download(filename=f'{audio.title}.mp3')
+                    buffer.seek(0)
+                    background_tasks.add_task(cleanup, file_path)
+                except RegexMatchError:
+                    return templates.TemplateResponse("index.html", {"request": request, "error": "Check the url"})
         else:
             return templates.TemplateResponse("index.html", {"request": request, "error": "Check the url"})
 
