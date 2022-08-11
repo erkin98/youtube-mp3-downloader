f2c00249e0895ab11dbda599f06b20f2a7c59c8d
Erkin <erkin.qarayev_contractor@gaintheory.com>
2022-08-11 19:28:16 +0400

    Add telegram support

--- app/main.py
+++ app/main.py
@@ -1,17 +1,60 @@
-from fastapi import Request, FastAPI
-from fastapi.templating import Jinja2Templates
+import os
 from pathlib import Path
-from fastapi.responses import HTMLResponse
+from io import BytesIO
+from pytube import YouTube
+from pytube.exceptions import RegexMatchError
+
+import telebot
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
 
 from .routers import streams
+from dotenv import load_dotenv, find_dotenv
 
+import logging
+
+# logger = telebot.logger
+# telebot.logger.setLevel(logging.DEBUG)
 app = FastAPI()
-
 
 app.include_router(streams.router)
 BASE_PATH = Path(__file__).resolve().parent
 templates = Jinja2Templates(directory=str(BASE_PATH / "templates"))
 
-@app.get("/", response_class=HTMLResponse)
-async def read_item(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "url": "Enter a url"})
+load_dotenv(find_dotenv(find_dotenv(os.path.join(BASE_PATH, '.env'))))
+API_KEY = os.getenv("API_KEY")
+bot = telebot.TeleBot(API_KEY)
+
+@app.get("/", response_class=RedirectResponse)
+async def redirect_stream():
+    return RedirectResponse("/streams")
+
+
+@bot.message_handler(commands=["start"])
+def greet(message):
+    bot.send_message(message.chat.id, "Enter the url and enjoy")
+
+
+@bot.message_handler(content_types=['text'])
+def hello(message):
+    if "youtu" in message.text:
+        buffer = BytesIO()  # Declaring the buffer
+        try:
+            url = YouTube(message.text)  # Getting the URL
+            audio = url.streams.filter(
+                mime_type="audio/mp4", abr="48kbps", only_audio=True
+            ).first()  # Store the video into a variable
+            file_path = audio.download()
+            buffer.seek(0)
+            bot.send_audio(message.chat.id, audio=open(file_path, 'rb'))
+            os.remove(file_path)
+            
+        except RegexMatchError:
+            bot.send_message(message.chat.id, "Wrong url")
+    else:
+        bot.send_message(message.chat.id, "Wrong url")
+        
+
+
+bot.polling()
--- app/routers/streams.py
+++ app/routers/streams.py
@@ -1,8 +1,7 @@
-import contextlib
 from fastapi import APIRouter, Request, BackgroundTasks
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
-from fastapi.responses import HTMLResponse,FileResponse,StreamingResponse
+from fastapi.responses import FileResponse
 from pytube import YouTube
 from pytube.exceptions import RegexMatchError
 from io import BytesIO
@@ -13,25 +12,39 @@ BASE_PATH = Path(__file__).resolve().parent.parent
 templates = Jinja2Templates(directory=str(BASE_PATH / "templates"))
 router = APIRouter()
 
+
 def cleanup(temp_file):
     os.remove(temp_file)
 
+
+@router.get("/streams")
+async def read_item(request: Request):
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "url": "Enter a url"}
+    )
+
+
 @router.post("/streams/{url}")
 async def read_users(request: Request, background_tasks: BackgroundTasks):
-    if request.method == "POST": 
+    if request.method == "POST":
         form = await request.form()
-        if (form["url"] and 'youtu' in form["url"]): 
-                buffer = BytesIO() # Declaring the buffer
-                try:
-                    url = YouTube(form["url"]) # Getting the URL
-                    audio = url.streams.filter(mime_type="audio/mp4", abr = '48kbps', only_audio = True).first() # Store the video into a variable
-                    file_path = audio.download(filename=f'{audio.title}.mp3')
-                    buffer.seek(0)
-                    background_tasks.add_task(cleanup, file_path)
-                except RegexMatchError:
-                    return templates.TemplateResponse("index.html", {"request": request, "error": "Check the url"})
+        if form["url"] and "youtu" in form["url"]:
+            buffer = BytesIO()  # Declaring the buffer
+            try:
+                url = YouTube(form["url"])  # Getting the URL
+                audio = url.streams.filter(
+                    mime_type="audio/mp4", abr="48kbps", only_audio=True
+                ).first()  # Store the video into a variable
+                file_path = audio.download(filename=f"{audio.title}.mp3")
+                buffer.seek(0)
+                background_tasks.add_task(cleanup, file_path)
+            except RegexMatchError:
+                return templates.TemplateResponse(
+                    "index.html", {"request": request, "error": "Check the url"}
+                )
         else:
-            return templates.TemplateResponse("index.html", {"request": request, "error": "Check the url"})
+            return templates.TemplateResponse(
+                "index.html", {"request": request, "error": "Check the url"}
+            )
 
     return FileResponse(file_path, filename=file_path)
-
